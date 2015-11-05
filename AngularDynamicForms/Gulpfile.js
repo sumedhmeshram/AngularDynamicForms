57ea007ab321595b3c326191447399677702ebb2
sumedh1980@gmail.com <sumedh1980@gmail.com>
2015-11-05 17:51:35 +0530

    First Commit

---
+++ AngularDynamicForms/Gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require('gulp');
+var bower = require('gulp-bower');
+var concat = require('gulp-concat');
+var karma = require('gulp-karma');
+
+gulp.task('bower', function () {
+    return bower('./bower_components');
+});
+
+gulp.task('copyLibs', ['bower'], function () {
+    gulp.src(['bower_components/angular/*.*'])
+        .pipe(gulp.dest('public/libs/angular'));
+
+    gulp.src(['bower_components/bootstrap/dist/css/*.*'])
+        .pipe(gulp.dest('public/libs/bootstrap'));
+
+    gulp.src(['bower_components/jquery/dist/*.*'])
+        .pipe(gulp.dest('public/libs/jquery'));
+});
---
+++ AngularDynamicForms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularDynamicForms.Startup))]
+namespace AngularDynamicForms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
